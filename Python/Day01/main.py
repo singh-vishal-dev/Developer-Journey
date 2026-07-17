191605e96a4af8a3e61b0549d8c8e34397814ab4
singh-vishal-dev <vishal170820@gmail.com>
2026-07-17 23:53:09 +0530

    day1 code added

--- Python/Day01/main.py
+++ Python/Day01/main.py
@@ -1,3 +1,4 @@
+# how to print any string in python
 print("Hello,Python Day01")
 print("Welcome to my 180-Day Developer Journey")
 print("My name is Vishal Singh")
@@ -6,3 +7,76 @@ print("I am pursuing B.tsch ")
 print(67+23)
 print(22*2)
 print(33-4)
+
+
+# single line commemt 
+'''this is my day-1.
+ it is multi-line comment'''
+
+
+# Variable-  A variable is a container in which we store data .
+# examples
+name= "vishal"
+age=20
+college="kipm technical campus"
+print(name)
+print(age)
+print(college)
+
+a=40
+b=20
+print("addition:",a+b)
+print("subtraction",a-b)
+print("multiplication:",a*b)
+print("devision:",a/b)
+
+
+# Data Types- a data type indicates the type of data store within avariable
+#examples
+age=20
+hight=5.6
+name="vishal"
+is_student=True
+print(age)
+print(type(age))
+print(hight)
+print(type(hight))
+print(name)
+print(type(name))
+print(is_student)
+print(type(is_student))
+
+
+#input()= input() is use to take data from the user
+#examples
+A=int(input("enter the first num:"))
+B=int(input("enter the second number:"))
+sum=A+B
+print("sub of A and B:",sum)
+
+name=input("enter your name:")
+print("welcome",name)
+age=int(input("enter your age:"))
+print("your age is :",age)
+
+city=input("your city name:")
+college=input("enter your college name:")
+print(city)
+print(college)
+
+x=int(input("enter the first num:"))
+y=int(input("enter the second num:"))
+print("addition:",x+y)
+print("subtraction:",x-y)
+print("multipication:",x*y)
+print("dvisoin:",x/y)
+
+
+
+
+
+
+
+
+ 
+
